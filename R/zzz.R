6d1f36d06d4fdcd114c0c09f1e6fbae722fe0550
liulin <needliulin@163.com>
2026-06-27 19:03:43 +0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,3 @@
-.onLoad <- function(libname, pkgname) {
-  cat("\n=============================================\n")
-  cat("Our story began in summer, a summer that knows no end.\n")
-  cat("best love\n")
-  cat("=============================================\n")
-}
-
 .onAttach <- function(libname, pkgname) {
   # 如果用户装了 crayon 包，就用彩色，没装就用普通文本
   if (requireNamespace("crayon", quietly = TRUE)) {
