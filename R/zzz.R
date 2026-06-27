427b1211be7f8a7a454e5c523cc490fc7c20ef12
liulin <needliulin@163.com>
2026-06-27 18:59:27 +0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -4,3 +4,17 @@
   cat("best love\n")
   cat("=============================================\n")
 }
+
+.onAttach <- function(libname, pkgname) {
+  # 如果用户装了 crayon 包，就用彩色，没装就用普通文本
+  if (requireNamespace("crayon", quietly = TRUE)) {
+    msg <- paste0(
+      crayon::green("Our story began in summer, a summer that knows no end.！\n"),
+      crayon::silver("Best love for best you"),
+      crayon::blue("dcz")
+    )
+    packageStartupMessage(msg)
+  } else {
+    packageStartupMessage("✅ lcz 包加载成功！")
+  }
+}
