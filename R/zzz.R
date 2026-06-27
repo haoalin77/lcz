13769511edccd2464844bc2232c0ba2f30144a2f
liulin <needliulin@163.com>
2026-06-27 17:11:28 +0800

    Create zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onLoad <- function(libname, pkgname) {
+  cat("\n=============================================\n")
+  cat("Our story began in summer, a summer that knows no end.\n")
+  cat("best love\n")
+  cat("=============================================\n")
+}
